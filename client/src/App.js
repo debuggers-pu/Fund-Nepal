d9f165c9e44fac25d4214cf9080328665d49e529
Sandesh <pdlsandesh144@gmail.com>
2022-09-20 15:24:41 +0545

    Herosection started

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Login from "./components/Login";
 import Signup from "./components/Signup"
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
+import HeroSection from "./pages/HeroSection";
 import {Toaster} from "react-hot-toast";
 
 
@@ -21,6 +22,7 @@ function App() {
 			<Navbar />
 			<Routes>
 				<Route path ="/" element={<HomePage/>}/>
+				<Route path ="/herosection" element={<HeroSection/>}/>
 				<Route path="/auth/login"  element={<Login/>}/>
 				<Route path="/auth/signup" element={<Signup/>}/>
 			</Routes>
