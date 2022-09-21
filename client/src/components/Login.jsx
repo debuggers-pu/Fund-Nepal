e25edf2d34d2d13261330ecfbb0504b672dd19aa
Kaji96 <tandesh96@gmail.com>
2022-09-21 13:41:48 +0545

    minor tweeks

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,70 +3,69 @@ import InputField from "./shared/Inputfield";
 import toast from "react-hot-toast";
 import { NavLink, useNavigate } from "react-router-dom";
 import { login } from "../axios";
-import { useDispatch } from "react-redux"
+import { useDispatch } from "react-redux";
 import { setAuthenticated, setUser } from "../redux/slices/authSlice";
 
 const Login = ({ onClick }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const dispatch = useDispatch();
-  const navigate = useNavigate()
-  const onLogin = async (e) => {
-    e.preventDefault();
-    try {
-      if (email !== "" && password !== "") {
-        const res = await login({ email, password });
-        if (res.status === 200) {
-          toast.success(res.data.message);
-          dispatch(setAuthenticated(true));
-          dispatch(setUser(res.data));
-          navigate("/")
-        }
-      } else {
-        toast.error("Please fill all the fields");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const onLogin = async e => {
+		e.preventDefault();
+		try {
+			if (email !== "" && password !== "") {
+				const res = await login({ email, password });
+				if (res.status === 200) {
+					toast.success(res.data.message);
+					dispatch(setAuthenticated(true));
+					dispatch(setUser(res.data));
+					navigate("/");
+				}
+			} else {
+				toast.error("Please fill all the fields");
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  return (
-    <div className="flex justify-center items-center">
-      <div className="m-auto p-10 mt-20 border-2 shadow-sm rounded-md">
-        <h1 className="font-bold text-2xl mb-4 text-center">
-          Sign in to{" "}
-          <span className="text-primaryColor italic">Fund Nepal</span>
-        </h1>
-        <InputField
-          placeholder={"Enter your email address"}
-          name="email"
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <InputField
-          placeholder={"Enter your password"}
-          name="password"
-          type={"password"}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+	return (
+		<div className="flex justify-center items-center">
+			<div className="m-auto p-10 mt-20 border-2 shadow-sm rounded-md">
+				<h1 className="font-bold text-2xl mb-4 text-center">
+					Login to <span className="text-primaryColor italic">Fund Nepal</span>
+				</h1>
+				<InputField
+					placeholder={"Enter your email address"}
+					name="email"
+					type="email"
+					onChange={e => setEmail(e.target.value)}
+				/>
+				<InputField
+					placeholder={"Enter your password"}
+					name="password"
+					type={"password"}
+					onChange={e => setPassword(e.target.value)}
+				/>
 
-        <NavLink to="/auth/signup">
-          <p
-            className="text-xs font-bold hover-effect mb-2 text-left "
-            onClick={onClick}
-          >
-            Don't have an account ? Register now.
-          </p>
-        </NavLink>
-        <div
-          className="text-sm font-bold px-4 py-2 mb-2 rounded-md bg-black text-white text-center hover:opacity-75 cursor-pointer"
-          onClick={onLogin}
-        >
-          Login
-        </div>
-      </div>
-    </div>
-  );
+				<NavLink to="/auth/signup">
+					<p
+						className="text-xs font-bold hover-effect mb-2 text-left "
+						onClick={onClick}
+					>
+						Don't have an account ? Register now.
+					</p>
+				</NavLink>
+				<div
+					className="text-sm font-bold px-4 py-2 mb-2 rounded-md bg-black text-white text-center hover:opacity-75 cursor-pointer"
+					onClick={onLogin}
+				>
+					Login
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import Searchbox from './shared/Searchbox'
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Searchbox from "./shared/Searchbox";
 
 const Navbar = () => {
-  return (
-    <div className='flex justify-between items-center bg-green-500 text-white py-2 px-10 '>
-        <h1 className='font-bold text-xl '>Fund Nepal</h1>
-        <Searchbox/>
-       <NavLink to='/auth/login'>
-       <button className='px-4 py-2 bg-white text-primaryColor hover:opacity-75 rounded-sm border-none'>Login</button>
-       </NavLink>
-    </div>
-  )
-}
+	return (
+		<div className="flex justify-between items-center bg-green-500 text-white py-2 px-10 ">
+			<NavLink to="/">
+				<h1 className="font-bold text-xl ">Fund Nepal</h1>
+			</NavLink>
 
-export default Navbar
+			<Searchbox />
+			<div className="space-x-3">
+				<NavLink to="/auth/login">
+					<button className="px-4 py-2 bg-white text-primaryColor hover:opacity-75 rounded-sm border-none">
+						Login
+					</button>
+				</NavLink>
+				<NavLink to="/auth/signup">
+					<button className="px-4 py-2 bg-white text-primaryColor hover:opacity-75 rounded-sm border-none">
+						Signup
+					</button>
+				</NavLink>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
