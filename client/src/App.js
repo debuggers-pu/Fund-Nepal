57382c6c03191fa46a8fc8cabd0233f04e792e98
Tsandesh <uchihazoro02@gmail.com>
2022-09-22 21:57:49 +0545

    router ma collected amount ani api hyantun

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,9 @@ import { Toaster } from "react-hot-toast";
 import CreatePost from "./pages/CreatePost";
 import { useSelector } from "react-redux";
 import Footer from "./pages/Footer";
-import AboutUs from "./pages/AboutUs";
-import HowItWorks from "./pages/HowItWorks";
 import PostDescription from "./pages/PostDescription";
 
 function App() {
-<<<<<<< HEAD
 	const modal = useSelector(state => state.modal.postModal);
 	return (
 		<>
@@ -30,36 +27,11 @@ function App() {
 				<Route path="/" element={<HomePage />} />
 				<Route path="/auth/login" element={<Login />} />
 				<Route path="/auth/signup" element={<Signup />} />
-				<Route path="/:id"></Route>{" "}
+				<Route path="/:id" element={<PostDescription />} />
 			</Routes>
 			<Footer />
 		</>
 	);
-=======
-  const modal = useSelector((state) => state.modal.postModal);
-  return (
-    <>
-      <Toaster
-        position="top-right"
-        reverseOrder={false}
-        toastOptions={{
-          className: "font-bold text-sm",
-        }}
-      />
-      {modal ? <CreatePost /> : ""}
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-		    <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="/howitworks" element={<HowItWorks />} />
-        <Route path="/postdescription" element={<PostDescription />} />
-        <Route path="/auth/login" element={<Login />} />
-        <Route path="/auth/signup" element={<Signup />} />
-      </Routes>
-	  <Footer />
-    </>
-  );
->>>>>>> 8082d5ccfa2c61a809f3d9ca6d105ee92f7403a5
 }
 
 export default App;
--- server/src/routes/payment-route.js
+++ server/src/routes/payment-route.js
@@ -2,43 +2,43 @@ const router = require("express").Router();
 const Post = require("../model/post-model");
 
 const stripe = require("stripe")(
-  "sk_test_51LkplHClMuxIph6nRuoVXkOG6SxDw78osa7pkdgH4uykvYxUhhs8jsfnN0qpsXyXHlPqe05ZO6f5SYLpxGClSRK700oS45RT8d"
+	"sk_test_51LkrgoSFgx4gzLZV3uJxt0LMfzCzhzJYufdsLBAbUliFbRdU76DhFfsxbY4idTz1Hqhg4U3cv4bNWux2oQTpgulG00zKMatJhE"
 );
 const { v4: uuidv4 } = require("uuid");
 
 router.post("/payment", (req, res) => {
-  const { token, amount, postid } = req.body;
-  const idempontencyKey = uuidv4();
+	const { token, amount, postid } = req.body;
+	const idempontencyKey = uuidv4();
 
-  return stripe.customers
-    .create({
-      email: token.email,
-      source: token.id,
-    })
-    .then((customer) => {
-      stripe.charges.create(
-        {
-          amount: amount * 100,
-          currency: "usd",
-          customer: customer.id,
-          receipt_email: token.email,
-          description: `purchase of ${postid}`,
-        },
-        { idempontencyKey }
-      );
-    })
-    .then((result) => {
-      res.status(200).json(result);
-      Post.findOne({ _id: postid })
-        .then((project) => {
-          project.amountCollected = project.amountCollected + parseInt(amount);
-          return res.status(201).json({ result });
-        })
-        .catch((err) => {
-          return res.status(404).json({ error: "User not found" });
-        });
-    })
-    .catch((err) => console.log(err));
+	return stripe.customers
+		.create({
+			email: token.email,
+			source: token.id,
+		})
+		.then(customer => {
+			stripe.charges.create(
+				{
+					amount: amount * 100,
+					currency: "usd",
+					customer: customer.id,
+					receipt_email: token.email,
+					description: `purchase of ${postid}`,
+				},
+				{ idempontencyKey }
+			);
+		})
+		.then(result => {
+			res.status(200).json(result);
+			Post.findOne({ _id: postid })
+				.then(project => {
+					project.amountCollected = project.amountCollected + parseInt(amount);
+					return res.status(201).json({ result });
+				})
+				.catch(err => {
+					return res.status(404).json({ error: "User not found" });
+				});
+		})
+		.catch(err => console.log(err));
 });
 
 module.exports = router;
